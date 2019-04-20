29a00f301c11d8c458c80b28c5d0d781329a45d9
Louis Murerwa <louis.b.murerwa.21@dartmouth.edu>
2019-04-20 19:46:24 -0400

    Added an on off button

--- background.js
+++ background.js
@@ -1,3 +1,28 @@
+//Setting badge of the extension.
+chrome.browserAction.setBadgeText({ text: 'OFF' });
+//Setting up a boolean variable to store the state of button
+var enable=false;
+//Storing state of button in local storage
+chrome.storage.sync.set({"enable": enable});
+//Responding to onclick action of the on/off button
+chrome.browserAction.onClicked.addListener(function (tab) {
+    enable = !enable;
+  //If button is on run the content script / extension
+    if (enable) {
+      chrome.browserAction.setBadgeText({ text: 'ON' });
+      chrome.tabs.executeScript(null, { file: 'content.js' });
+    } else {
+//Reload the page and dont run the script
+      chrome.browserAction.setBadgeText({ text: 'OFF' });
+      chrome.tabs.executeScript(tab.id, {code: 'window.location.reload();'});
+    }
+//Restore the current state of the button
+    chrome.storage.sync.set({"enable": enable}, function() {
+      console.log('Value is set to ' + enable);
+    });
+  });
+
+
 var getCommentsRepoURL = function () {
     return 'https://raw.githubusercontent.com/capJavert/clippy-dictionary/master/clippy.json?v=' + new Date().getTime()
 }
@@ -8,7 +33,7 @@ var loadComments = function () {
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
            settings.comments = JSON.parse(xhttp.response);
-
+            console.log("kfkkf");
             browser.tabs.query({}, function(tabs) {
                 for (var index in tabs) {
                     browser.tabs.sendMessage(
--- content.js
+++ content.js
@@ -1,20 +1,27 @@
+//The outer statement checks if the button is in the on state before executing
 
-alert("Hello from your Chrome extension!");
+chrome.storage.sync.get("enable", function(result) {
+    console.log('Value currently is ' + result.enable)
+    if (result.enable) {
+    console.log("CalledContent");
+        alert("Hello from your Chrome extension!");
 
-var div = document.createElement("div"); 
-var imgPath = chrome.extension.getURL('img/clippy.png');
-div.innerHTML = `
-    <img id="clippy" src="`+ imgPath+ `"/>
-`;
-document.body.appendChild(div); 
-
-const url = chrome.runtime.getURL('quotes.json');
-
-fetch(url)
-    .then((response) => response.json()) //assuming file contains json
-    .then((json) => {
-        console.log(json)
+        var div = document.createElement("div"); 
+        var imgPath = chrome.extension.getURL('img/clippy.png');
+        div.innerHTML = `
+            <img id="clippy" src="`+ imgPath+ `"/>
+        `;
+        document.body.appendChild(div); 
         
-    });
-
-console.log(json);
+        const url = chrome.runtime.getURL('quotes.json');
+        
+        fetch(url)
+            .then((response) => response.json()) //assuming file contains json
+            .then((json) => {
+                console.log(json)
+                
+            });
+        
+        console.log(json);
+    }
+  });
